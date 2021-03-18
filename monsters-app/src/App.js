50d16278abb9fcbbe1c123c468a6d8bc06a9ea08
Temo Abesadze <abesadzetemo1997@gmail.com>
2021-03-18 16:28:00 +0400

    Finished monsters app

--- monsters-app/src/App.js
+++ monsters-app/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+
+    // this.handleChange = this.handleChange.bind(this);
+    // ეს აღარაა საჭირო თუ ეროუ ფუნქციას ვიყენებთ handleChange როცა შემოდის
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        console.log('users', users)
+        this.setState({ monsters: users })
+      }
+      )
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+
+    // this is Object destructuring and is the easiest way:
+    const { monsters, searchField } = this.state;
+
+    // Alternatives:
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField; 
+
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search monsters"
+          handleChange={this.handleChange} />
+
+        {/* this.setState({ searchField: e.target.value }, () => console.log(this.state)) */}
+
+        {/* this.setState() -ს გადავცემთ მეორე არგუმენტს, მეორე ფუნქციას, ქოლბექს. რადგან setState() ასინქრონულია */}
+        {/* და მაშინ დაილოგოს e.target.value, როცა setState() დაფინიშდება  */}
+
+        {/* და არა ისე როგორც ქვემოთ წერია: */}
+        {/* console.log(this.state)   */}
+
+        <CardList monsters={filteredMonsters} />
+
+      </div>
+    )
+  }
 }
 
 export default App;
